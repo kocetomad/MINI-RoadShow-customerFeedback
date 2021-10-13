3dc75b937899126a8c4be4b4b8874eabc1e7e14b
Alexander Petrov (FG-773) <Alexander.Petrov@bmwgroup.com>
2021-10-13 17:19:06 +0100

    added listener

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+const { Console } = require('console')
 const express = require('express')
 const app = express()
 
 app.use(express.static('public'))
 //yash chage
-app.listen(3000)
+app.listen(3000, () => console.log("APP RUNNING ON PORT:3000"))
