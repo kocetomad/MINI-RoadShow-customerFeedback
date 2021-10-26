7afa5fffdfdc306989b9ebf26d223cef72be5507
Alexander Petrov <Alexander.Petrov@bmwgroup.com>
2021-10-26 16:15:02 +0100

    db insert backend

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const { Console } = require('console')
 const express = require('express')
+const { Pool, Client } = require('pg')
+const queries = require('SqlQuerys.js')
+
+const client = new Client({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'postgres',
+  password: '123456',
+  port: 5432,
+})
+client.connect()
+
+
+
 const app = express()
 
 app.use(express.static('Pages'))
+app.post('/insertScores', (req, res) => controllers.InsertScores(req, res, client))
+
 
 //yash chage
 app.listen(3000, () => console.log("APP RUNNING ON PORT:3000"))
